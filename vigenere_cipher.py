ecdd6434e372ff127e0c328280d639b9e5563656
gerikah <aldaygerikah@gmail.com>
2023-04-17 16:04:07 +0800

    pseudocode

---
+++ vigenere_cipher.py
@@ -0,0 +1,15 @@
+# Object Oriented Programming CMPE-103 LAB EXERCISE No. 1 : Problem 3
+
+
+# Convert message and keyword to uppercase
+# Convert the keyword to a numeric key of values 0 - 25
+# Empty ciphertext string
+    # Loop through message characters
+        # Get letter value of current message character
+        # Get corresponding letter value of current keyword character
+        # Calculate sum and take mod 26
+        # Convert sum_val to corresponding letter and add to ciphertext list
+    # Join ciphertext list into a string and return
+# Input message
+# Input keyword
+# Print the ciphertext
